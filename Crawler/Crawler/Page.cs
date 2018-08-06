3da551f318bac183f474c3cfa8fd4637f529399c
Chao <chaoaretasty@gmail.com>
2018-08-06 19:07:36 +0100

    Initial Version
    Implements basic fetching and parsing of links from a page

---
+++ Crawler/Crawler/Page.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Crawler
+{
+    public class Page
+    {
+        public Uri Url { get; set; }
+        public String Title { get; set; }
+        public IEnumerable<Uri> InLinks { get; set; }
+        public IEnumerable<Uri> OutLinks { get; set; }
+        public CrawlResult Result { get; set; }
+    }
+}
---
+++ Crawler/Crawler/UrlSanitiser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    public class UrlSanitiser
+    {
+		private readonly string _localHostname;
+		private readonly string _scheme;
+		private Uri _baseUri;
+
+		public UrlSanitiser(string localHostname, string scheme)
+		{
+			_localHostname = localHostname;
+			_scheme = scheme;
+			_baseUri = new Uri(_scheme + "://" + _localHostname);
+		}
+
+		public Uri SanitiseLocal(string url)
+		{
+			if(url == null) { return null; }
+
+			var fragmentPos = url.IndexOf('#');
+			if(fragmentPos > -1)
+			{
+				url = url.Remove(fragmentPos);
+			}
+
+			Uri uri;
+
+			if (Uri.TryCreate(url, UriKind.Relative, out uri))
+			{
+				return new Uri(_baseUri, url);
+			}
+
+			if(Uri.TryCreate(url, UriKind.Absolute, out uri))
+			{
+				return (uri.Authority == _baseUri.Authority) ? uri : null;
+			}
+			
+			return null;
+		}
+    }
+}
